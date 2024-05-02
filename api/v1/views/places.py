dc76072f3ad9fbecc1d49b24f1c3d0fbe388afb4
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-05-02 05:09:18 -0700

    places update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 """
-route for handling Place objects and operations
+Place views for the API.
 """
+
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.place import Place
-
 
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
                  strict_slashes=False)
@@ -20,7 +21,6 @@ def places_by_city(city_id):
         place_list.append(obj.to_json())
 
     return jsonify(place_list)
-
 
 @app_views.route("/cities/<city_id>/places", methods=["POST"],
                  strict_slashes=False)
@@ -50,7 +50,6 @@ def place_create(city_id):
 
     return resp
 
-
 @app_views.route("/places/<place_id>",  methods=["GET"],
                  strict_slashes=False)
 def place_by_id(place_id):
@@ -66,7 +65,6 @@ def place_by_id(place_id):
         abort(404)
 
     return jsonify(fetched_obj.to_json())
-
 
 @app_views.route("/places/<place_id>",  methods=["PUT"],
                  strict_slashes=False)
@@ -93,7 +91,6 @@ def place_put(place_id):
     fetched_obj.save()
 
     return jsonify(fetched_obj.to_json())
-
 
 @app_views.route("/places/<place_id>",  methods=["DELETE"],
                  strict_slashes=False)
